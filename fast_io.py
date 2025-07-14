4ec28c05c9311f90e3cc575641bd2a8e8bb22e65
Airhood <shinesky0530@gmail.com>
2025-07-14 12:20:39 +0900

    fast IO

---
+++ fast_io.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline.rstrip('\r\n')
+
+
+import os, io, sys
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+
+
+import sys
+print = sys.stdout.write # 줄바꿈 없음
+
+
+import sys, os, io, atexit
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+
+
+# 최종
+import sys, os, io, atexit
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
